57b36e62f6715dbd12c9de3f6b4116b81014e211
Rabea Gleissner <rabeagleissner@gmail.com>
2020-12-12 12:26:27 +0800

    Ensure that API response has the correct format

--- lib/services/hotel_supplier_service.rb
+++ lib/services/hotel_supplier_service.rb
@@ -7,7 +7,14 @@ module Services
     HOTEL_SUPPLIER_URL = 'http://www.mocky.io/v2/5d99f2ac310000720097da14'
 
     def retrieve_hotel_prices
-      Request.get(HOTEL_SUPPLIER_URL)[:hotels]
+      hotel_prices = Request.get(HOTEL_SUPPLIER_URL)
+      unless hotel_prices.key?(:hotels)
+        return []
+      end
+
+      hotel_prices[:hotels].select do |hotel_price|
+        (hotel_price.keys & [:id, :price]).length == 2
+      end
     end
   end
 end
--- spec/services/hotel_supplier_service_spec.rb
+++ spec/services/hotel_supplier_service_spec.rb
@@ -4,14 +4,31 @@ require 'services/hotel_supplier_service'
 
 describe Services::HotelSupplierService do
   let(:hotel_supplier_service) { Services::HotelSupplierService.new }
-  let(:hotel_prices) { { hotels: [{ id: 'ab12', price: 100 }, { id: 'xy98', price: 200 }] } }
-
-  before(:each) do
-    allow(Services::Request).to receive(:get).and_return(hotel_prices)
-  end
 
   it 'returns an array of ids and prices' do
+    hotel_prices = { hotels: [{ id: 'ab12', price: 100 }, { id: 'xy98', price: 200 }] }
+    allow(Services::Request).to receive(:get).and_return(hotel_prices)
+
     response = hotel_supplier_service.retrieve_hotel_prices
+
     expect(response).to eq([{ id: 'ab12', price: 100 }, { id: 'xy98', price: 200 }])
+  end
+
+  it 'returns empty array when API response does not have the expected hotel key' do
+    badly_formed_response = { buildings: [{ id: 'ab12', price: 100 }, { id: 'xy98', price: 200 }] }
+    allow(Services::Request).to receive(:get).and_return(badly_formed_response)
+
+    response = hotel_supplier_service.retrieve_hotel_prices
+
+    expect(response).to eq([])
+  end
+
+  it 'removes inclomplete hashes' do
+    badly_formed_response = { hotels: [{ id: 'ab12' }, { price: 3 }, { id: 'xy98', price: 200 }] }
+    allow(Services::Request).to receive(:get).and_return(badly_formed_response)
+
+    response = hotel_supplier_service.retrieve_hotel_prices
+
+    expect(response).to eq([{ id: 'xy98', price: 200 }])
   end
 end
