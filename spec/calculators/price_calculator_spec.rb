ce67af91d01c2b2a2afec39ef0114498d4eb3ac7
Rabea Gleissner <rabeagleissner@gmail.com>
2020-12-13 08:27:45 +0800

    Add unit tests for price calculator

---
+++ spec/calculators/price_calculator_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'calculators/price_calculator'
+
+describe Calculators::PriceCalculator do
+  let(:price_calculator_factory) { instance_spy(Calculators::PriceCalculatorFactory) }
+  let(:standard_price_calculator) { instance_spy(Calculators::StandardPriceCalculator) }
+
+  before(:each) do
+    allow(Calculators::PriceCalculatorFactory).to receive(:new).and_return(price_calculator_factory)
+    allow(price_calculator_factory).to receive(:create).and_return(standard_price_calculator)
+  end
+
+  it 'calls factory to create the right price calculator' do
+    price_calculator = Calculators::PriceCalculator.new('B')
+    price_calculator.calculate([])
+
+    expect(Calculators::PriceCalculatorFactory).to have_received(:new).with('B')
+  end
+
+  it 'calls price calculator to calculate' do
+    prices = []
+
+    price_calculator = Calculators::PriceCalculator.new('B')
+    price_calculator.calculate(prices)
+
+    expect(standard_price_calculator).to have_received(:calculate).with(prices)
+  end
+end
